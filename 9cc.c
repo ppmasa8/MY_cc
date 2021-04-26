a6260acf53991e4bee8c56d212693e5f2a8fdf9d
ppmasa8 <kiyokiyopip56@gmail.com>
2021-04-26 23:56:36 +0900

    middle comit

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,71 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	TK_RESERVED, //記号
+	TK_NUM,      //整数トークン
+	TK_EOF,      //入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+	TokenKind kind; //トークンの型
+	Token *next;    //次の入力トークン
+	int val;        //kindがTK_NUMの場合、その数値
+	char *str;      //トークン文字列
+};
+
+//現在着目しているトークン
+Token *token;
+
+//エラーを報告するための関数
+//printfと同じ引数をとる
+void error(char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+//次のトークンが期待している記号の時には、トークンを一つ読み進めて
+//信を返す。それ以外の場合には偽を返す。
+bool consume(char op) {
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		return false;
+	token = token->next;
+	return true;
+}
+
+//次のトークンが期待している記号の時には、トークンを１つ読み進める。
+//それ以外の場合にはエラーを報告する。
+void expect(char op) {
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		error("'%c'ではありません", op);
+	token = token->next;
+}
+
+//次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+//それ以外の場合にはエラーを報告する。
+int expect_number() {
+	if (token->kind != TK_NUM)
+		error("数ではありません");
+	int val = token->val;
+	token = token->next;
+	return val;
+}
+
+bool at_eof() {
+	return token->kind == TK_EOF;
+}
+
+
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
